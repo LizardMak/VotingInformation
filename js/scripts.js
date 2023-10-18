359c99d5472d78cb5c14fbd6130ec967d8971a91
Hunter Empey <LizardMak@gmail.com>
2023-10-18 13:26:50 -0600

    fix both links showing

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,13 @@ window.onload = function() {
     e.preventDefault();
     if (inputAge >= 18) {
       let voterInfo = document.getElementById("legalAge");
+      let clownMusic = document.getElementById("illegalAge");
+      clownMusic.setAttribute("class", "hidden")
       voterInfo.removeAttribute("class");
     } else {
       let clownMusic = document.getElementById("illegalAge");
+      let voterInfo = document.getElementById("legalAge");
+      voterInfo.setAttribute("class", "hidden")
       clownMusic.removeAttribute("class");
     }
   }
